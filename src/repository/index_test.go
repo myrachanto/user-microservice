193c927a2690193664b658f86dd2cd5fac98076f
myrachanto <myrachanto@gmail.com>
2022-02-12 11:51:53 +0300

    mysql reasonnig

--- src/repository/index_test.go
+++ src/repository/index_test.go
@@ -6,8 +6,10 @@ import (
 	// "github.com/myrachanto/ddd/httperors"
 )
 
+//this test is a little crazy given the github actions does not ... asssigning a name to created mysql docker container
 func TestGetconnected(t *testing.T) {
 	// GormDB, err := IndexRepo.Getconnected()
 	// assert.NotNil(t, GormDB, "The conncetion passed")
 	// assert.Nil(t, err, "The error must be nil for successifully database connection")
+	//
 }
